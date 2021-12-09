27fd99e25d5b3147b4ab6abcafbdb9515903fb58
xiaogeng <359315634@qq.com>
2021-12-09 14:43:05 +0800

    完成购物车

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,16 @@
 const getters = {
-	cartLength(state) {
+	cartLength (state) {
 		return state.cartList.length
 	},
-	cartList(state) {
+	cartList (state) {
 		return state.cartList
+	},
+	checkCartLength (state) {
+		return state.cartList.filter(res => {
+			return res.checked
+		}).reduce((prev, cur) => {
+			return prev + cur.count
+		}, 0)
 	}
 }
 
---
+++ src/views/cart/childComps/CartList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cart-list">
+    <scroll class="content" :data="[cartList]" ref="scroll">
+      <cart-list-item
+        v-for="item in cartList"
+        :key="item.iid"
+        :item-info="item"
+      ></cart-list-item>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from 'components/common/scroll/Scroll'
+import CartListItem from './CartListItem'
+
+export default {
+  name: 'CartList',
+  components: {
+    Scroll,
+    CartListItem
+  },
+  props: {
+    cartList: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  activated() {
+    this.$refs.scroll.refresh()
+  }
+}
+</script>
+
+<style scoped>
+.cart-list {
+  height: calc(100vh - 44px);
+}
+.content {
+  position: absolute;
+  top: 44px;
+  left: 0;
+  right: 0;
+  bottom: 93px;
+
+  height: calc(100% - 93px - 44px);
+  overflow: hidden;
+}
+</style>
